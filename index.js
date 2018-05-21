5b10c2e828231c52544193798d5c13cc78ece6bd
Yan Wang <wangyan6783@gmail.com>
2018-05-21 20:38:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if (cardNumber) {
     var totalPrice = `$` + total();
+    cart = [];
     return `Your total cost is ${totalPrice}, which will be charged to the card ${cardNumber}.`;
   } else {
     return `Sorry, we don't have a credit card on file for you.`;
